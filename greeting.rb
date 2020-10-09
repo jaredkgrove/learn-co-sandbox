a6030bf9249e87e05d68efd3ad3665d2a8583e97
Jared Grove <jaredkgrove@gmail.com>
2020-10-09 18:58:08 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,41 @@
+require "pry"
+class Dog
+  attr_accessor :name, :owner
+end 
+
+class Owner
+  attr_accessor :name
+end
+
+class Person
+  attr_accessor :partner, :name
+ 
+  def initialize(name)
+    @name = name
+  end
+ 
+  def get_married(person)
+    self.partner = person
+    if person.class != Person 
+      begin
+        raise PartnerError
+      rescue PartnerError => error
+          puts error.message
+      end
+    else
+      person.partner = self
+    end
+  end
+ 
+  class PartnerError < StandardError
+      def message 
+    "you must give the get_married method an argument of an instance of the person class!"
+  end
+  end
+end
+ 
+beyonce = Person.new("Beyonce")
+beyonce.get_married("Jay-Z")
+puts beyonce.name
+
+#binding.pry
